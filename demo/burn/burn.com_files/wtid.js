d61bba7a50cecd2ccef7a906b5bc689921808cdc
Armando Braun <armando@plyfe.me>
2014-05-22 12:33:12 -0400

    Demo for burn energy drink

---
+++ demo/burn/burn.com_files/wtid.js
@@ -0,0 +1,3 @@
+if (typeof(Webtrends.dcss.dcsobj_0.dcsGetIdCallback)=="function"){
+Webtrends.dcss.dcsobj_0.dcsGetIdCallback({"gWtId":"86bea175-137a-48c6-af22-26ae346139d6","gTempWtId":"","gWtAccountRollup":"1"});
+}
